1f59233335d3edf8d186725b92086a6a477a0801
Kohl Kohlbrenner <edwin.kohlbrenner@gmail.com>
2014-09-17 09:05:06 -0400

    updated form details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,13 @@
 class ArticlesController < ApplicationController
   def show
+  	@article=Article.find(params[:id])
+
+  	@breaking_articles=Article.all.order(created_at: :desc)
+  	
   end
 
   def new
+  	@article=Article.new
   end
 
   def index
@@ -10,4 +15,16 @@ class ArticlesController < ApplicationController
 
   def edit
   end
+
+def create
+	@article=Article.create(article_params)
+		redirect_to article_path(@article)
+end
+
+private
+
+def article_params
+	params.require(:article).permit(:title,:author,:content,:category)
+end
+
 end
